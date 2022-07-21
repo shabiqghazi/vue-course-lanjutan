238f7576060afcbc842af94e170409310c204c33
shabiqghazi <sgarkaan02@gmail.com>
2022-07-21 22:04:04 +0700

    37,38 Slot & Slot Name

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container mb-5">
-    <h1 class="mb-4">Components</h1>
+    <!-- <h1 class="mb-4">Components</h1> -->
     <!-- <greet-component name="Shabiq Ghazi Arkaan" college-name="UIN Sunan Gunung Djati Bandung" />
     <greet-component name="Hendra Setiawan" college-name="Universitas Indonesia" />
     <greet-component :name="name" :college-name="collegeName" /> -->
@@ -9,7 +9,39 @@
     <!-- <component-c /> -->
     <!-- <button class="btn btn-primary mb-3" @click="showPopup = true">show popup</button>
     <popup-component v-show="showPopup" @close="closePopup" /> -->
-    <input-component v-model="name" />
+    <!-- <input-component v-model="name" /> -->
+    <h1 class="mb-4">Slots</h1>
+    <div class="col-5">
+      <card-component></card-component>
+      <card-component>
+        <p>Shabiq</p>
+      </card-component>
+      <card-component>
+        <h4>Ghazi Arkaan</h4>
+      </card-component>
+      <card-component>
+        <img
+          src="https://img-9gag-fun.9cache.com/photo/a3Q5VW5_700bwp.webp"
+          alt=""
+          width="250"
+        />
+      </card-component>
+      <card-component>
+        <template v-slot:card-header>
+          <h2>Card Header A</h2>
+        </template>
+        <template v-slot:default>
+          <img
+            src="https://img-9gag-fun.9cache.com/photo/a3Q5VW5_700bwp.webp"
+            alt=""
+            width="250"
+          />
+        </template>
+        <template v-slot:card-footer>
+          <h4>Card Footer A</h4>
+        </template>
+      </card-component>
+    </div>
   </div>
 </template>
 
@@ -18,7 +50,8 @@
 // import ArticleComponent from "./components/ArticleComponent.vue";
 // import ComponentC from './components/ComponentC.vue'
 // import PopupComponent from './components/PopupComponent.vue'
-import InputComponent from "./components/Input.vue";
+// import InputComponent from "./components/Input.vue";
+import CardComponent from "./components/Card.vue";
 
 export default {
   name: "App",
@@ -27,15 +60,15 @@ export default {
     // ArticleComponent,
     // ComponentC,
     // PopupComponent,
-    InputComponent,
+    // InputComponent,
+    CardComponent,
   },
   data() {
     return {
       // name: 'Anthony Sinisuka Ginting',
       // collegeName: 'Institut Teknologi Bandung',
-
       // showPopup: false,
-      name: "",
+      // name: "",
     };
   },
   provide() {
---
+++ src/components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card shadow mb-3" style="max-width: 18rem">
+    <div class="card-header">
+      <slot name="card-header"></slot>
+    </div>
+    <div class="card-body">
+      <slot></slot>
+    </div>
+    <div class="card-footer">
+      <slot name="card-footer"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardComponent",
+};
+</script>
+
+<style scoped>
+</style>
